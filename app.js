082affabcbefd01666cfba0dcbdf9a0375c2e558
Rob Culliton <rculliton@yieldbot.com>
2013-11-15 21:31:47 -0500

    use config module for port settings, etc...

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-var express = require('express');
+'use strict';
+
+var express = require('express'),
+  http = require('http'),
+  settings = require('config');
 
 var app = express();
+
+app.set('port', settings.port)
 
 app.get('/', function(req, res) {
   res.send('welcome to wampum');
 });
 
-app.listen(3000);
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
