eddd16d1acc5ee95a5e46495f5b9b2b9b24b7a10
Rob Culliton <rculliton@yieldbot.com>
2013-11-17 21:44:45 -0500

    be sure to put static middleware before catch all ui route, check bookmarked page for reference

--- app.js
+++ app.js
@@ -2,14 +2,18 @@
 
 var express = require('express'),
   http = require('http'),
-  settings = require('config');
+  path = require('path'),
+  settings = require('config'),
+  ui = require('./lib/routes/ui');
 
 var app = express();
 
 app.set('port', settings.port);
 // use dev to get the nice colored styling for http requests
-app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, 'static')));
+
+app.use(express.logger('dev'));
+app.use(ui.app);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
