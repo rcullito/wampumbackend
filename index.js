1367a0386ad644ed4695e9e68fd51e8666ab6162
Rob Culliton <rculliton@yieldbot.com>
2014-07-06 10:40:45 -0400

    cut over search route to hapi

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var Hapi = require("hapi"),
   Joi = require("joi");
 
 var location = require('./lib/hapiroutes/location'),
+  search = require('./lib/hapiroutes/search'),
   tags = require('./lib/hapiroutes/tags');
 
 var server = new Hapi.Server(4010, "localhost");
 
 server.route(location.route);
+server.route(search.route);
 server.route(tags.route);
 
 server.start(function() {
