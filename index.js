ab26900a42b0e55bcb1ce8ffb6c94f6b6ef92f1c
Rob Culliton <rculliton@yieldbot.com>
2014-07-06 12:25:10 -0400

    never ring the bell! fully dropped express and over to hapi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Hapi = require("hapi"),
 
 var location = require('./lib/hapiroutes/location'),
   search = require('./lib/hapiroutes/search'),
+  ui = require('./lib/hapiroutes/ui'),
   tags = require('./lib/hapiroutes/tags');
 
 var server = new Hapi.Server(4010, "localhost");
@@ -10,6 +11,8 @@ var server = new Hapi.Server(4010, "localhost");
 server.route(location.route);
 server.route(search.route);
 server.route(tags.route);
+server.route(ui.routes);
+
 
 server.start(function() {
     console.log("Hapi server started @", server.info.uri);
