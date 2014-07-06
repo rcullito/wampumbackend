39b0941843b3f51ca8b7b5fea56f0b5438f23621
Rob Culliton <rculliton@yieldbot.com>
2014-07-06 09:50:26 -0400

    split out tags route into its own hapi route!

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Hapi = require("hapi"),
   Joi = require("joi");
 
-var location = require('./lib/hapiroutes/location');
+var location = require('./lib/hapiroutes/location'),
+  tags = require('./lib/hapiroutes/tags');
 
 var server = new Hapi.Server(4010, "localhost");
 
 server.route(location.route);
+server.route(tags.route);
 
 server.start(function() {
     console.log("Hapi server started @", server.info.uri);
