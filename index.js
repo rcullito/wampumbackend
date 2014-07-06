bcd8ec10bed416584435341f167e4ec1837c11a2
Rob Culliton <rculliton@yieldbot.com>
2014-07-06 09:20:40 -0400

    making changes to the life im living

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var Hapi = require("hapi"),
   Joi = require("joi");
 
-var animal = require('./lib/hapiroutes/animal'),
-  user = require('./lib/hapiroutes/user');
+var location = require('./lib/hapiroutes/location');
 
 var server = new Hapi.Server(4010, "localhost");
 
-server.route(animal.route);
-server.route(user.route);
+server.route(location.route);
 
 server.start(function() {
     console.log("Hapi server started @", server.info.uri);
