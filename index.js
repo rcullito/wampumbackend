e8f1b0834832a3f9837466ebb8a9bcdbfbb23899
Rob Culliton <rculliton@yieldbot.com>
2014-07-12 21:58:36 -0400

    wraper server in module.parents conditional. use server methods to cache tags!

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ var Hapi = require("hapi"),
   Good = require('good'),
   Joi = require("joi");
 
-var location = require('./lib/hapiroutes/location'),
-  search = require('./lib/hapiroutes/search'),
-  ui = require('./lib/hapiroutes/ui'),
-  tags = require('./lib/hapiroutes/tags');
-
 
 var server = new Hapi.Server(8888);
 
-server.route(location.route);
-server.route(search.route);
-server.route(tags.route);
-server.route(ui.routes);
+module.exports = server;
 
-var goodOptions = {
-  extendedRequests: true,
-  subscribers: {
-      'console': ['request', 'log', 'error'],
-  }
-};
+if (!module.parent) {
 
-var plugins = [
-  {plugin: Good, options: goodOptions}
-];
+  var location = require('./lib/hapiroutes/location'),
+    search = require('./lib/hapiroutes/search'),
+    ui = require('./lib/hapiroutes/ui'),
+    tags = require('./lib/hapiroutes/tags');  
 
-server.pack.register(plugins, function (err) {
-  server.start(function() {
-      console.log("Hapi server started @", server.info.uri);
+  var goodOptions = {
+    extendedRequests: true,
+    subscribers: {
+        'console': ['request', 'log', 'error'],
+    }
+  };
+
+  var plugins = [
+    {plugin: Good, options: goodOptions}
+  ];
+
+  server.route(location.route);
+  server.route(search.route);
+  server.route(tags.route);
+  server.route(ui.routes);
+
+  server.pack.register(plugins, function (err) {
+    server.start(function() {
+        console.log("Hapi server started @", server.info.uri);
+    });
   });
-});
+}
